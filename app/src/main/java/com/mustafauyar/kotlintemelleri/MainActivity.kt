8615b0c9814852ce967cddcac1a53718d60937b2
Mustafa2 <Mustafauyar91@gmail.com>
2023-12-19 12:51:26 +0300

    gereksiz sinif kaldirildi sonunda println yazabildim

--- app/src/main/java/com/mustafauyar/kotlintemelleri/MainActivity.kt
+++ app/src/main/java/com/mustafauyar/kotlintemelleri/MainActivity.kt
@@ -1,33 +1,10 @@
 package com.mustafauyar.kotlintemelleri
 
-import android.os.Bundle
-import androidx.activity.ComponentActivity
-import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.mustafauyar.kotlintemelleri.ui.theme.KotlinTemelleriTheme
-
-class MainActivity : ComponentActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContent {
-            KotlinTemelleriTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    Greeting("Android")
-                }
-            }
-        }
-    }
-}
 
 @Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
@@ -35,12 +12,18 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
         text = "Hello $name!",
         modifier = modifier
     )
+    println("lokman")
+    println(5*8)
+    var x = 10
+    var y = 20
+    var z = 30
+    println("$x  y ise $y")
 }
 
-@Preview(showBackground = true)
+@Preview(showBackground = false)
 @Composable
 fun GreetingPreview() {
     KotlinTemelleriTheme {
-        Greeting("Android")
+        Greeting("world".uppercase())
     }
 }
